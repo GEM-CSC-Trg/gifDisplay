64946dbed749c412a33ce35abc2ae5071cb32f21
Valeria D'Amante <valeriadamante@gmail.com>
2025-05-26 19:24:22 +0200

    fixed env.sh

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 apply_cmssw_customization_steps() {
     # run_cmd git cms-init
     # run_cmd git cms-addpkg Validation/MuonCSCDigis
-    run_cmd mkdir -p GifDisplay
-    run_cmd ln -s "$ANALYSIS_PATH/GifDisplay" GifDisplay
+    run_cmd mkdir -p gifDisplay
+    run_cmd ln -s "$ANALYSIS_PATH/GifDisplay" gifDisplay/
 }
 
 CMSSW_VERSION="CMSSW_13_0_3"
