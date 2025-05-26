1b6a10bb2edaa4fd684118ccfacb39b378caedf1
Valeria D'Amante <valeriadamante@gmail.com>
2025-05-26 18:57:57 +0200

    integration in common tool

---
+++ env.sh
@@ -0,0 +1,21 @@
+# apply_cmssw_customization_steps() {
+#     run_cmd git cms-init
+#     run_cmd git cms-addpkg Validation/MuonCSCDigis
+#     run_cmd mkdir -p GEMCode
+#     run_cmd ln -s "$this_dir/GEMValidation" GEMCode/GEMValidation
+# }
+
+CMSSW_VERSION="CMSSW_14_2_0_pre1"
+
+action() {
+    local this_file="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
+    local this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
+    local this_file_path="$this_dir/$(basename $this_file)"
+    export ANALYSIS_PATH="$this_dir"
+    echo "Running action with CMSSW version: $CMSSW_VERSION"
+    echo source $ANALYSIS_PATH/GEM-CSC-trg-dev/env.sh "$this_file_path" "$CMSSW_VERSION" "$@"
+    source $ANALYSIS_PATH/GEM-CSC-trg-dev/env.sh "$this_file_path" "$CMSSW_VERSION" "$@"
+}
+action "$@"
+unset -f action
+# unset -f apply_cmssw_customization_steps
